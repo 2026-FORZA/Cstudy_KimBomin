58b16cb76830543d093763d36fde3cea05069170
bominkim0517 <bominkim0517@sookmyung.ac.kr>
2026-05-06 18:48:15 +0900

    1_Lab04

---
+++ 1_Lab04_1.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		printf("%d ", i);
+}
---
+++ 1_Lab04_2.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
